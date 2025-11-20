fb084b31db4f7c36e62b090ba066b0ef485bae04
DZODZODZI  CEDRICK <cdzodzodzi@st.ug.edu.gh>
2025-11-20 09:35:34 +0000

    Improved HashTable functionality and collision handling
    Refactor HashTable methods for better functionality and readability.

--- HashTable/index.js
+++ HashTable/index.js
@@ -3,25 +3,71 @@ const castStringToInt = (string, tableSize) => {
   for (let i = 0; i < string.length; i++) {
     hashkey = (hashkey * 31 + string.charCodeAt(i)) % tableSize;
   }
-
   return hashkey;
 };
 
 class HashTable {
   table = new Array(50);
+
   setItem = (key, val) => {
     const idx = castStringToInt(key, this.table.length);
 
     if (!this.table[idx]) {
       this.table[idx] = [[key, val]];
     } else {
+      // update key if exists
+      for (let i = 0; i < this.table[idx].length; i++) {
+        if (this.table[idx][i][0] === key) {
+          this.table[idx][i][1] = val;
+          return;
+        }
+      }
       this.table[idx].push([key, val]);
     }
   };
+
   getItem = (key) => {
     const idx = castStringToInt(key, this.table.length);
     if (!this.table[idx]) return null;
-    return this.table[idx].find((x) => x[0] === key)[1];
+
+    const found = this.table[idx].find((x) => x[0] === key);
+    return found ? found[1] : null;
+  };
+
+  removeItem = (key) => {
+    const idx = castStringToInt(key, this.table.length);
+    if (!this.table[idx]) return;
+
+    this.table[idx] = this.table[idx].filter((item) => item[0] !== key);
+
+    // if bucket becomes empty, set it to null
+    if (this.table[idx].length === 0) {
+      this.table[idx] = undefined;
+    }
+  };
+
+  keys = () => {
+    const keys = [];
+    for (let bucket of this.table) {
+      if (bucket) {
+        for (let [key] of bucket) {
+          keys.push(key);
+        }
+      }
+    }
+    return keys;
+  };
+
+  values = () => {
+    const values = [];
+    for (let bucket of this.table) {
+      if (bucket) {
+        for (let [, value] of bucket) {
+          values.push(value);
+        }
+      }
+    }
+    return values;
   };
 
   print = () => {
@@ -31,41 +77,4 @@ class HashTable {
       }
     }
   };
-
-  removeItem = (key) => {
-    const idx = castStringToInt(key, this.table.length);
-    if (!this.table[idx]) return null;
-    delete this.table[idx];
-  };
-
-  keys = () => {
-    const keys = [];
-    for (let i = 0; i < this.table.length; i++) {
-      if (this.table[i]) {
-        keys.push(this.table[i][0][0]);
-      }
-    }
-    return keys;
-  };
-
-  values = () => {
-    const values = [];
-    for (let i = 0; i < this.table.length; i++) {
-      if (this.table[i]) {
-        values.push(this.table[i][0][1]);
-      }
-    }
-    return values;
-  };
 }
-
-// Test
-const myTable = new HashTable();
-myTable.setItem("firstName", "qbentil");
-myTable.setItem("lastName", "shadow");
-myTable.setItem("age", 10);
-myTable.setItem("address", "Pokuase");
-myTable.setItem("city", "Accra");
-myTable.setItem("state", "Greater Accra");
-
-console.log(myTable.values());
